ae3fcbf72115c97c083aa349d439a29034c6f387
jerehellstrom <72606337+jerehellstrom@users.noreply.github.com>
2022-04-09 23:46:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-var http = require("http");
-http.createServer(function (request, response) {
-
-    // Send the HTTP header. HTTP Status: 200 = OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/html'});
-
-    const express = require('express');
+   const express = require('express');
     const app = express();
     const path = require('path');
     const router = express.Router();
+    const port = 3000;
     
     router.get('/',function(req,res){
       res.sendFile(path.join(__dirname+'/index.html'));
@@ -33,11 +27,3 @@ http.createServer(function (request, response) {
     app.listen(process.env.port || 3000);
     
     console.log('HTML now Running at Port 3000');
-
-  }).listen(8081);
-
-  // Console will print the message - Will attempt to fix the port thing if i have time
-  console.log('Server running at http://127.0.0.1:8081/ - Go to port 8081 in the browser if you do not get port 3000 message in command prompt');
-
-  
-  
