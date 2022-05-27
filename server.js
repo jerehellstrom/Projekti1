bfce9bd0ddc7964bb4e5691f7392e8100c724965
jerehellstrom <72606337+jerehellstrom@users.noreply.github.com>
2022-05-27 15:41:53 +0300

    pieniä parannuksia

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
     const app = express();
     const path = require('path');
     const router = express.Router();
-    
+
     router.get('/',function(req,res){
       res.sendFile(path.join(__dirname+'/index.html'));
       //__dirname : It will resolve to your project folder.
@@ -11,6 +11,13 @@
     router.get('/guestbook',function(req,res){
       res.sendFile(path.join(__dirname+'/guestbook.html'));
     });
+
+
+    //guestbook.html needs this directory to exist in order to fetch the data from the json file
+    router.get('/guestbook.json',function(req,res){
+      res.sendFile(path.join(__dirname+'/guestbook.json'));
+    });
+    
     
     router.get('/newmessage',function(req,res){
       res.sendFile(path.join(__dirname+'/newmessage.html'));
@@ -24,5 +31,5 @@
     app.use('/', router);
     app.use(express.static('public'));
     app.listen(process.env.PORT || 8080);
-    
+
     console.log('HTML now Running at Port 8080');
